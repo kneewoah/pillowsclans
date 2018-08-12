3d250f20c80f1ce022792633065d67961ef7c93e
kneewoah <42323086+kneewoah@users.noreply.github.com>
2018-08-12 19:41:46 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -116,7 +116,7 @@ client.on("message", async message => {
   if(command === "iw") {
       let time = args[0];
       let drop = args[1];
-      message.channel.send(`An Iron Wizard spawned for ${time} minutes and dropped a(n) ${drop}`);
+      message.channel.send(`**Iron Wizard** \n Time Started: NA \n Time Ended: NA \n Drop: ${drop}`);
   }
 });
 
