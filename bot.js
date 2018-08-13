983e7edcecdcf554b71ec06b7d65607ec2c8da11
kneewoah <42323086+kneewoah@users.noreply.github.com>
2018-08-13 17:19:05 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -71,14 +71,14 @@ client.on("message", async message => {
       let dropUp = drop.charAt(0).toUpperCase() + drop.slice(1);
       
       if(rarity === "red") {
-         let color = "#e81f09"
+         let color = "#e81f09";
           } else if(rarity === "white") {
-              let color = "#ffffff"
+              let color = "#ffffff";
               } else if(rarity === "gold") {
-                  let color = "#e8d109"
+                  let color = "#e8d109";
                   } else if(rarity === "blue") {
-                      let color = "#7be0f7"
-                      } else return message.channel.send("Please enter a valid rarity color: blue, white, gold, or red"
+                      let color = "#7be0f7";
+                      } else message.channel.send("Please enter a valid rarity color: blue, white, gold, or red";
                       
       const embed = new Discord.RichEmbed()
         .setTitle("**Iron Wizard**")
