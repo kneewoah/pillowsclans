7e5cc06a53ab7e4d92ab086cf9faeb41de907684
kneewoah <42323086+kneewoah@users.noreply.github.com>
2018-08-13 11:20:22 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -68,9 +68,33 @@ client.on("message", async message => {
       let time = args[0];
       let drop = args[1];
       client.channels.get("478347833842335765").send({embed: {
-  color: 3447003,
-  description: "A very simple Embed!"
-      }});
+    color: 3447003,
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },
+    title: "This is an embed",
+    url: "http://google.com",
+    description: "This is a test embed to showcase what they look like and what they can do.",
+    fields: [{
+        name: "Fields",
+        value: "They can have different fields with small headlines."
+      },
+      {
+        name: "Masked links",
+        value: "You can put [masked links](http://google.com) inside of rich embeds."
+      },
+      {
+        name: "Markdown",
+        value: "You can put all the *usual* **__Markdown__** inside of them."
+      }
+    ],
+    timestamp: new Date(),
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: "© Example"
+    }
+  }
   }
 });
 
