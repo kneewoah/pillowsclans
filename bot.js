258dd6df600c44bed99fd1a669052c305cfd5a36
kneewoah <42323086+kneewoah@users.noreply.github.com>
2018-08-13 16:50:23 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -68,7 +68,7 @@ client.on("message", async message => {
       let color = args[0];
       let drop = args[1];
       let time = args[2];
-      let dropUp = drop.charAt(0).toUpperCase();
+      let dropUp = drop.charAt(0).toUpperCase() + drop.slice(1);
       const embed = new Discord.RichEmbed()
         .setTitle("**Iron Wizard**")
         .setAuthor(`Logged by ${message.author.username}`, "")
