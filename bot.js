52d7e37e25df1c5ce7ee5cb9f17002c35bad42c2
kneewoah <42323086+kneewoah@users.noreply.github.com>
2018-08-12 22:12:04 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('I am ready!');
     client.user.setStatus("dnd")
-    client.ueer.setActivity("Mineplex Clans")
+    client.user.setActivity("Mineplex Clans")
 });
 
 client.on('message', message => {
