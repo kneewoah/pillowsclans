4b95ea2e119ece320e4c6ba8c5174b933f757a52
kneewoah <42323086+kneewoah@users.noreply.github.com>
2018-08-13 10:44:10 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require("./config.json");
 
-client.on('ready') () => {
+client.on("ready") () => {
     console.log('I am ready!');
 });
 
