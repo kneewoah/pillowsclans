fcce108a8c6d6516cc7575e7e53f8b568b2d6486
kneewoah <42323086+kneewoah@users.noreply.github.com>
2018-08-14 14:02:04 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -74,7 +74,7 @@ client.on("message", async message => {
       
       const dropUp = drop.charAt(0).toUpperCase() + drop.slice(1);
       
-      let color = if(rarity === "red") "config.red";
+      const color = if(rarity === "red") "config.red";
           
       const embed = new Discord.RichEmbed()
         .setTitle("**Iron Wizard**")
