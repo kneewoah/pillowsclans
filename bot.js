44d5dbae499f07fbad87dc038f31cccd31d0b252
kneewoah <42323086+kneewoah@users.noreply.github.com>
2018-08-14 18:02:01 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -123,7 +123,7 @@ client.on("message", async message => {
         .setTitle("**Skeleton King**")
         .setAuthor(`Logged by ${message.author.username}`)
         // Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-        .setColor()
+        .setColor(color)
         .setFooter("The demonic Skeleton King has been slain!")
         // Takes a Date object, defaults to current date.
         .setTimestamp()
