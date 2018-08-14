1ed51fb6a25f682a321ed7a8ca6209dfb9b32eb3
kneewoah <42323086+kneewoah@users.noreply.github.com>
2018-08-14 17:55:38 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -123,7 +123,7 @@ client.on("message", async message => {
         .setTitle("**Skeleton King**")
         .setAuthor(`Logged by ${message.author.username}`)
         // Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-        .setColor(color)
+        .setColor()
         .setFooter("The demonic Skeleton King has been slain!")
         // Takes a Date object, defaults to current date.
         .setTimestamp()
@@ -155,7 +155,7 @@ client.on("message", async message => {
         .setTitle("**Capture Point**")
         .setAuthor(`Logged by ${message.author.username}`)
         // Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-        .setColor(color)
+        .setColor()
         .setFooter(`${message.author.username} has captured the point!`)
         // Takes a Date object, defaults to current date.
         .setTimestamp()
