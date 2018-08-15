e249b42f5a6b5c7b66b9d9cd0c33476d84da54c6
kneewoah <42323086+kneewoah@users.noreply.github.com>
2018-08-15 19:15:48 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -256,6 +256,7 @@ if(command === "ping") {
          const sayMessage = args.join(" ");
          message.delete().catch(O_o=>{}); 
          message.channel.send(sayMessage);
+     }
   //filler
 });
 
