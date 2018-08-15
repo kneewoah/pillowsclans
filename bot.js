3c28827ceae542f9077a18f4b1a7041ce2fd31aa
kneewoah <42323086+kneewoah@users.noreply.github.com>
2018-08-15 18:39:01 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on("ready", () => {
 });
 
 client.on("message", async message => {
-  if(message.author.bot || message.content.indexOf(config.prefix) !== 0 |) return;
+  if(message.author.bot || message.content.indexOf(config.prefix) !== 0) return;
 
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
