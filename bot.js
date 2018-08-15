b35d0c42f56126afe62bd2c30cf018590220b47c
kneewoah <42323086+kneewoah@users.noreply.github.com>
2018-08-15 13:53:03 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -43,7 +43,7 @@ client.on("message", async message => {
       
       if(message.member.roles.find("name", "Muted")) {
          message.reply(rb + " is already muted you mormon.");
-      } else if(message.author === message.mentions.members.first()) {
+      } else if(message.author.id === rb.id) {
          return message.reply("YOU'RE FUCKING RETARDED");
       } else {
          message.reply(rb + " has been muted.");
