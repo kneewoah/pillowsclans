ecdc0768a1ec7aa6f7737f31b4ad235c1b955c6e
kneewoah <42323086+kneewoah@users.noreply.github.com>
2018-08-15 14:00:21 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,6 +51,20 @@ client.on("message", async message => {
       }
   }
     
+  if(command === "unmute") {
+      let rb = message.mentions.members.first();
+      let roleID = message.guild.roles.find("name", "Muted").id;
+      
+      if(!message.member.roles.find("name", "Muted")) {
+         message.reply(rb + " isn't even muted you mormon.");
+      } else if(message.author.id === rb.id) {
+         return message.reply("if you were muted you wouldnt be able to type this command. And if you are muted but CAN TYPE, then you're ADMIN and you cna LITERALLY EDIT YOUR ROLES");
+      } else {
+         message.reply(rb + " has been unmuted.");
+         rb.removeRole(roleID);
+      }
+  }
+    
     
   if(command === "purge") {
     const deleteCount = parseInt(args[0], 10);
