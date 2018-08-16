6cfb551d66c6e1dc34dd830c5bfc01007f83e30f
kneewoah <42323086+kneewoah@users.noreply.github.com>
2018-08-16 13:58:48 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -259,8 +259,12 @@ if(message.member.hasPermission("MANAGE_MESSAGES" || "ADMINISTRATOR") || message
      // Dice
      if(command === "roll") {
        
-         let sides = parseInt(args[0], 10);
-          
+         let sides;
+         if(args[0]) {
+           let sides = parseInt(args[0], 10);
+         } if(!args[0]) {
+           let sides = "6"
+         }
          const roll = Math.floor(Math.random() * sides) + 1;
          message.channel.send("🔹      |   **Rolling**...   |      🔹\n**==================**\n➡️ | 🎲⚫️⚫️⚫️⚫  | ⬅️")
              .then((msg)=>{setTimeout(function() {
